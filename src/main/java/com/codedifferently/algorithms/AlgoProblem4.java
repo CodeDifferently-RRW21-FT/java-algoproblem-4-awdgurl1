bd70de756c053ad05e0c1f378936065b2ba0343f
Ashley Orelien <ashleyorelien@Ashleys-MacBook-Air.local>
2022-01-11 08:37:32 -0500

    Version 1

--- src/main/java/com/codedifferently/algorithms/AlgoProblem4.java
+++ src/main/java/com/codedifferently/algorithms/AlgoProblem4.java
@@ -1,10 +1,22 @@
 package com.codedifferently.algorithms;
+import java.util.*;
 
 public class AlgoProblem4 {
 
     public String encrypt(String message, int shift){
 
-        return null;
 
-    }
+        System.out.println("Please enter your message");
+        Scanner sc = new Scanner (System.in);
+        String input = sc.next();
+        for (int i = 0; i < input.length; i++) {
+            int numericValue = Character.getNumericValue(i);
+            numericValue = i + 3;
+            char  ch = (char)numericValue;
+            String output = String.valueOf(ch);
+            System.out.println(output);
+            return output;
+        }
+    } 
 }
+
