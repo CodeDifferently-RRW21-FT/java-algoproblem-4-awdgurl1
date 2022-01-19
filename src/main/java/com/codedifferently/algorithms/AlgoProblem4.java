2241a2356decb765e394e87603f16baee05b2743
Ashley Orelien <ashleyorelien@Ashleys-MacBook-Air.local>
2022-01-18 20:41:07 -0500

    Version 2

--- src/main/java/com/codedifferently/algorithms/AlgoProblem4.java
+++ src/main/java/com/codedifferently/algorithms/AlgoProblem4.java
@@ -1,38 +1,20 @@
-//Location of file
 package com.codedifferently.algorithms;
-//Import all files in java.util
-import java.util.*;
-//Publicly accessible instructions to create objects of 'AlgoProblem4'
+
 public class AlgoProblem4 {
 
-    //public String encrypt(String message, int shift) {
-    public static void main(String[] args) {
+    public String encrypt(String message, int shift) {
 
-
-        //pseudocode
-        //Prompting the user for input using Scanner
-        //Store input as a string variable
-        //Iterate through each character in the string variable using charAt
-        //Get the ascii of each character in the string variable
-        //Add 3 to the value of the ascii (*x-z)
-        //Convert the new ascii back to a character
-        //Create a string variable that will hold the new characters
-        //Print the string variable
-
-        System.out.println("Please enter your message");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        String output = null;
-        for (int i = 0; i < input.length(); i++) {
-            char characterReceived = input.charAt(i);
-            if(characterReceived >= 'a' && characterReceived <= 'z'){
-             int ascii = (int) characterReceived;
-             ascii = ascii + 3;
-             char conversion = (char)ascii;
-             conversion++;
-                System.out.print(conversion);
+        String output = "";
+        char backToChar;
+        char characterReceived;
+        for (int i = 0; i < message.length(); i++) {
+             characterReceived = message.charAt(i);
+            if (Character.isLetter(characterReceived)) {
+                int ascii = characterReceived + shift;
+                backToChar = (char) ascii;
+                output += String.valueOf(backToChar);
             }
-        }// return output;
+        }return output;
     }
 }
 
