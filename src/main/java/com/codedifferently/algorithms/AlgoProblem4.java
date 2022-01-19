07001e2a303aec538bc317cf3ff84bb3fcef8768
Ashley Orelien <ashleyorelien@Ashleys-MacBook-Air.local>
2022-01-19 17:36:24 -0500

    Version 3

--- src/main/java/com/codedifferently/algorithms/AlgoProblem4.java
+++ src/main/java/com/codedifferently/algorithms/AlgoProblem4.java
@@ -5,14 +5,14 @@ public class AlgoProblem4 {
     public String encrypt(String message, int shift) {
 
         String output = "";
-        char backToChar;
         char characterReceived;
+        char ascii;
         for (int i = 0; i < message.length(); i++) {
              characterReceived = message.charAt(i);
             if (Character.isLetter(characterReceived)) {
-                int ascii = characterReceived + shift;
-                backToChar = (char) ascii;
-                output += String.valueOf(backToChar);
+                ascii = (char) (characterReceived + shift);
+                output += ascii;
+
             }
         }return output;
     }
